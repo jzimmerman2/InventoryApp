3ff3468089751e1500c37dc651e34729b10d1367
Jacob Zimmerman <jzimmerman710@gmail.com>
2025-04-27 13:54:06 -0400

    saving work, figured out popup menu

--- app/src/main/java/com/inventory/mainactivity/MainActivity.kt
+++ app/src/main/java/com/inventory/mainactivity/MainActivity.kt
@@ -2,11 +2,16 @@ package com.inventory.mainactivity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
+import androidx.compose.ui.graphics.painter.Painter
 import com.inventory.directoryactivity.DirectoryActivity
 
 import com.inventory.listactivity.ListActivity
@@ -54,13 +59,20 @@ class MainActivity : ComponentActivity() {
 
     fun setUpDirectoryButton(directoryButton: Button) : Button {
         directoryButton.setOnClickListener {
-            val toOpenDirectory = Intent(this, DirectoryActivity::class.java)
-            startActivity(toOpenDirectory)
+            //val toOpenDirectory = Intent(this, DirectoryActivity::class.java)
+            //startActivity(toOpenDirectory)
+            val popup = PopupMenu(this, directoryButton)
+
+            popup.apply {
+                menuInflater.inflate(R.menu.popup_menu, menu)
+            }
+
+            popup.show()
         }
-        directoryButton.setOnLongClickListener(View.OnLongClickListener() {
+        /*directoryButton.setOnLongClickListener(View.OnLongClickListener() {
             Toast.makeText(applicationContext, "long click works", Toast.LENGTH_SHORT).show()
             true
-        })
+        })*/
         return directoryButton
     }
 
